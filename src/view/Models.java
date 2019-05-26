f22a603894c54d7020d89716e2fbdf3ded4a6c08
Fabian <43959083+HyFabi@users.noreply.github.com>
2019-05-26 10:20:57 +0200

    k

---
+++ src/view/Models.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.scene.image.Image;
+import model.Card;
+
+public class Models {
+	Card c;
+	Image i;
+	
+	public Models(Card c) {
+		this.c = c;
+	}
+	
+	public void giveImage() {
+		
+	}
+}
